20b3f8e60f29560e902aea3b167f378ab24e473a
riedelcastro <sebastian.riedel@gmail.com>
2015-04-05 13:08:39 +0100

    better sentence segmentation and default tokenizers and segmenters

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/RegexTokenizer.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/RegexTokenizer.scala
@@ -1,13 +1,15 @@
 package uk.ac.ucl.cs.mr.statnlpbook
 
 import com.google.common.base.Splitter
-import ml.wolfe.nlp.{CharOffsets, Token, Document}
+import ml.wolfe.nlp.{Sentence, CharOffsets, Token, Document}
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ArrayBuffer
+import scala.util.matching.Regex
 
 /**
  * @author riedel
  */
-class GuavaTokenSplitter(val splitter: Splitter) extends (Document => Document) {
+class RegexTokenizer(val splitter: Splitter) extends (Document => Document) {
   import scala.collection.mutable.ArrayBuffer
 
   def apply(doc: Document) = {
@@ -34,17 +36,55 @@ class GuavaTokenSplitter(val splitter: Splitter) extends (Document => Document) 
 }
 
 object Tokenizer {
-  def fromRegEx(regex:String) = new GuavaTokenSplitter(Splitter.onPattern(regex).omitEmptyStrings())
+  def fromRegEx(regex:String) = new RegexTokenizer(Splitter.onPattern(regex).omitEmptyStrings())
+
+  lazy val default = {
+    val punct = "[\\.\\?]"
+    val keepPeriod = "(Mr|Mrs|Dr)"
+    val beforePunct = s"(?<!$keepPeriod)(?=$punct)"
+    val afterPunct = s"(?<=$punct)(?!\\s)" //but not if whitespace follows
+
+    val tokenizer = Tokenizer.fromRegEx(s"(\\s|$beforePunct|$afterPunct)")
+    tokenizer
+  }
 
   def main(args: Array[String]) {
     val punct = "[\\.\\?]"
     val beforePunct = s"(?=$punct)"
     val afterPunct = s"(?<=$punct)(?!\\s)" //but not if whitespace follows
-    val tokenizer = Tokenizer.fromRegEx(s"(\\s|$beforePunct|$afterPunct)")
     val doc = Document.fromString("Thinkin' of a master plan Mr. Peko. ")
     val splitter = Splitter.onPattern(s"(\\s|$beforePunct|$afterPunct)")
 //    val splitter = Splitter.onPattern("(?<!Mr)(?=[\\.,])")
     println(splitter.split("Thinkin' of.a master plan Mr. and Mrs. Peko.").asScala.mkString("\n"))
-    println(tokenizer(doc))
+    println(default(doc))
   }
+}
+
+class RegexSegmenter(val regex:Regex) extends (Document => Document) {
+
+  def apply(doc: Document) = {
+    val sentences = new ArrayBuffer[Sentence]
+
+    def split(sentence:Sentence):Seq[Sentence] = {
+      val result = new ArrayBuffer[Sentence]()
+      var current = new ArrayBuffer[Token]
+      for (tok <- sentence.tokens) {
+        current += tok
+        if (regex.findFirstIn(tok.word).isDefined) {
+          result += Sentence(current)
+          current = new ArrayBuffer[Token]
+        }
+      }
+      if (current.nonEmpty) result += Sentence(current)
+      result.toSeq
+    }
+    doc.copy(sentences = doc.sentences.flatMap(split))
+  }
+}
+
+object Segmenter {
+
+  lazy val default = fromRegEx("^[\\.;?]$")
+
+  def fromRegEx(regex:String) = new RegexSegmenter(regex.r)
 }
