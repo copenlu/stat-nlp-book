5783a2439133b602397769654c13e1d9950bad0d
riedelcastro <sebastian.riedel@gmail.com>
2015-04-07 06:39:15 +0100

    ngram language models

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/chapter/LanguageModels.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/chapter/LanguageModels.scala
@@ -1,8 +1,262 @@
 package uk.ac.ucl.cs.mr.statnlpbook.chapter
 
+import cc.factorie.variable.DenseProportions1
+import ml.wolfe.nlp.Document
+
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.util.Random
+
+import uk.ac.ucl.cs.mr.statnlpbook.corpora.OHHLA
+
+
 /**
  * @author riedel
  */
 object LanguageModels {
+
+  type History = List[String]
+  type Vocab = Set[String]
+
+
+  trait Counts extends (String => Double) {
+    val random = new Random(0)
+
+    def vocab: Vocab
+
+    def apply(word: String): Double
+
+    def inverse(count: Double): List[String]
+
+    def activeCounts: List[Double]
+
+    def normalizer: Double
+
+    def prob(word: String) =
+      this(word) / normalizer
+
+    lazy val indexedVocab = vocab.toIndexedSeq
+    lazy val proportions = new DenseProportions1((indexedVocab map prob).toArray)
+
+    def sample() = {
+      indexedVocab(proportions.sampleIndex(random))
+    }
+
+
+  }
+
+  class LaplaceCounts(val original: Counts, val alpha: Double) extends Counts {
+
+    def apply(word: String): Double = original(word) + alpha
+
+    def inverse(count: Double) = original.inverse(count - alpha)
+
+    def vocab = original.vocab
+
+    lazy val activeCounts = original.activeCounts map (_ + alpha)
+
+    lazy val normalizer = original.normalizer + vocab.size * alpha
+
+  }
+
+  class RawCounts(val counts: Map[String, Double], val vocab: Vocab) extends Counts {
+
+    def apply(word: String) = counts.getOrElse(word, 0.0)
+
+    def inverse(count: Double) = inverseMap(count)
+
+    lazy val activeCounts = 0.0 :: counts.values.toList
+    lazy val inverseMap = counts.groupBy(_._2) mapValues (_.keys.toList)
+    lazy val normalizer = counts.valuesIterator.sum
+  }
+
+  trait DiscountedCounts extends Counts {
+    def original: Counts
+
+    def discounts: Map[Double, Double]
+
+    def apply(word: String) = discounts(original(word))
+
+    lazy val inverseDiscounts = discounts.groupBy(_._2) mapValues (_.keys.toList)
+
+    def inverse(count: Double) = inverseDiscounts(count) flatMap original.inverse
+
+    lazy val activeCounts = original.activeCounts map (c => discounts.getOrElse(c, c))
+
+    lazy val normalizer = {
+      val old = original.normalizer
+      old - discounts.keys.map(count => inverse(count).length * (count + discounts(count))).sum
+    }
+  }
+
+  def vocabLM(vocabulary: Vocab) = {
+    new NGramLM {
+      val counts = Map(List.empty[String] -> constantCounts(vocabulary))
+
+      def historySize = 0
+
+      def vocab = vocabulary
+    }
+  }
+
+  def constantCounts(vocabulary: Vocab) = new LaplaceCounts(new RawCounts(Map.empty, vocabulary), 1.0)
+
+  def ngramLM(train: History, historyLength: Int, vocabulary: Vocab): NGramLM = {
+    val counts = new mutable.HashMap[History, mutable.HashMap[String, Double]]
+    @tailrec
+    def recurse(data: History): Unit = data match {
+      case head :: tail if tail.take(historyLength).length == historyLength =>
+        if (vocabulary(head)) {
+          val history = tail.take(historyLength)
+          val map = counts.getOrElseUpdate(history, new mutable.HashMap[String, Double])
+          map(head) = map.getOrElse(head, 0.0) + 1.0
+        }
+        recurse(tail)
+      case _ =>
+    }
+    recurse(padded(train,historyLength))
+    val map = counts mapValues (c => new RawCounts(c.toMap, vocabulary))
+    new NGramLM {
+      val counts = map.toMap withDefaultValue constantCounts(vocabulary)
+
+      def historySize = historyLength
+
+      def vocab = vocabulary
+    }
+  }
+
+  def laplace(lm: NGramLM, addCount: Double) = new NGramLM with LMDecorator {
+    val counts = lm.counts mapValues (c => new LaplaceCounts(c, addCount))
+
+    def original = lm
+  }
+
+
+  //for each history we need to know which values are defined
+  //type Counts = PartialFunction[String,Double]
+  //PartialFunction[History,PartialFunction[String,Double]]
+
+  trait NGramLM {
+    def counts: Map[History, Counts]
+
+    def vocab: Vocab
+
+    lazy val indexedVocab = vocab.toIndexedSeq
+
+    def historySize: Int
+
+    lazy val padding = (0 until historySize).toList map (_ => PAD)
+
+    @tailrec
+    final def sampleMany(numSamples: Int, soFar: History = padding): History = {
+      require(soFar.length >= historySize)
+      numSamples match {
+        case 0 => soFar
+        case n =>
+          val history = soFar.take(historySize)
+          val next = sample(history)
+          sampleMany(n - 1, next :: soFar)
+      }
+    }
+
+    @tailrec
+    final def logPerplexity(data: History, result: Double = 0.0): Double = {
+      require(data.length >= historySize)
+      data match {
+        case d if d.length == historySize =>
+          result
+        case head :: tail if vocab(head) =>
+          val p = prob(head, tail.take(historySize))
+          val l = math.log(p)
+          logPerplexity(tail, result + l)
+        case head :: tail =>
+          logPerplexity(tail, result)
+      }
+    }
+
+    def perplexity(data: History) = {
+      val logP = logPerplexity(padded(data,historySize))
+      val normed = -logP / data.length
+      math.exp(normed)
+    }
+
+    def prob(word: String, history: History) =
+      counts(history).prob(word)
+
+    def sample(history: History) = {
+      counts(history).sample()
+    }
+
+
+  }
+
+  trait LMDecorator extends NGramLM {
+    def original: NGramLM
+
+    def historySize = original.historySize
+
+    def vocab = original.vocab
+  }
+
+  def history(docs: Iterable[Document], padding: Int = 5) = {
+    val content = docs flatMap (_.tokens map (_.word))
+    content.toList.reverse
+  }
+
+  @tailrec
+  def replaceFirstOccurenceWithOOV(oov: String,
+                                   toProcess: History,
+                                   result: History = Nil,
+                                   vocab: Vocab = Set(PAD)): History = {
+    toProcess match {
+      case Nil => result
+      case head :: tail =>
+        val (word, newVocab) = if (vocab(head)) (head, vocab) else (oov, vocab + head)
+        replaceFirstOccurenceWithOOV(oov, tail, word :: result, newVocab)
+    }
+  }
+
+  def padded(history: History, howMuch:Int = 5) = {
+    val init = (0 until howMuch).toList map (_ => PAD)
+    history ++ init
+  }
+
+  def filterByVocab(vocab: Vocab, oov: String, corpus: History) =
+    corpus map (w => if (vocab(w)) w else oov)
+
+  def OOV = "<OOV>"
+
+  def PAD = "<PAD>"
+
+  def main(args: Array[String]) {
+    //when calculating perplexity and training a model, the input should always be padded
+    val docs = OHHLA.JLive.allAlbums flatMap OHHLA.loadDir
+    val (trainDocs, testDocs) = docs.splitAt(docs.length - 1)
+    val train = replaceFirstOccurenceWithOOV(OOV, history(trainDocs))
+    val vocab = train.toSet
+    val test = filterByVocab(vocab, OOV, history(testDocs))
+
+    println(train.length)
+
+    //    println(words(OHHLA.loadAll(OHHLA.jLiveAllAbove)).take(100).mkString("\n"))
+    //    println("---")
+    //    println(test.take(10).mkString("\n"))
+
+    for (_ <- 0 until 1) {
+      val lms = Seq(
+        "vocabLM" -> vocabLM(vocab),
+        "unigramLM" -> ngramLM(train, 0, vocab),
+        "bigramLM" -> ngramLM(train, 1, vocab)
+      )
+
+      for ((name, lm) <- lms) {
+        println(name)
+        println(lm.perplexity(test))
+        println(lm.sampleMany(10).reverse)
+
+      }
+    }
+
+  }
 
 }
--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/corpora/OHHLA.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/corpora/OHHLA.scala
@@ -1,8 +1,10 @@
 package uk.ac.ucl.cs.mr.statnlpbook.corpora
 
 import java.io.File
+import java.nio.charset.MalformedInputException
 
 import ml.wolfe.nlp.Document
+import ml.wolfe.util.Util
 import uk.ac.ucl.cs.mr.statnlpbook.{Segmenter, Tokenizer}
 
 import scala.io.Source
@@ -11,29 +13,63 @@ import scala.io.Source
  * @author riedel
  */
 object OHHLA {
-  def main(args: Array[String]) {
-    val file = new File("data/ohhla/www.ohhla.com/anonymous/j_live/allabove/satisfy.jlv.txt.html")
+
+  lazy val tokenizer = {
+    val punct = "[\\.\\?,]"
+    val keepPeriod = "(Mr|Mrs|Dr)"
+    val beforePunct = s"(?<!$keepPeriod)(?=$punct)"
+    val afterPunct = s"(?<=$punct)(?!\\s)" //but not if whitespace follows
+    val contractions = "('s|'re|'m|'d|'t|'ll)"
+    val brackets = "(?<=\\()|(?=\\))"
+    Tokenizer.fromRegEx(s"(\\s|(?=<)|(?<=>)|$beforePunct|$afterPunct|(?=$contractions)|$brackets)")
+  }
+
+  lazy val segmenter = Segmenter.fromRegEx("^</BAR>$")
+
+  lazy val pipeline = tokenizer andThen segmenter
+
+  lazy val root = new File("data/ohhla/www.ohhla.com/anonymous/")
+
+  object JLive {
+    lazy val AllAbove = new File(root, "j_live/allabove/")
+    lazy val BestPart = new File(root, "j_live/bestpart/")
+    lazy val albumRoot = new File(root, "j_live")
+    lazy val allAlbums = albumRoot.listFiles().filter(_.isDirectory)
+
+  }
+
+
+
+  def load(file: File) = {
     val lines = Source.fromFile(file).getLines().mkString("\n")
     val start = lines.indexOf("<pre>") + "<pre>".length
     val end = lines.indexOf("</pre>")
     val headerAndLyrics = lines.slice(start,end)
     val lyrics = headerAndLyrics.split("\n").drop(6).map(_.trim).mkString("<BAR>","</BAR><BAR>","</BAR>")
-    //println(headerAndLyrics)
-    //println(lyrics)
-    val punct = "[\\.\\?,]"
-    val keepPeriod = "(Mr|Mrs|Dr)"
-    val beforePunct = s"(?<!$keepPeriod)(?=$punct)"
-    val afterPunct = s"(?<=$punct)(?!\\s)" //but not if whitespace follows
-//    val tokenizer = Tokenizer.fromRegEx(s"(\\s|$beforePunct|$afterPunct)")
-    val tokenizer = Tokenizer.fromRegEx("(\\s|(?=<)|(?<=>))")
-    val segmenter = Segmenter.fromRegEx("^</BAR>$")
-    val pipeline = tokenizer andThen segmenter
-
     val doc = Document.fromString(lyrics)
-    val processed = pipeline(doc)
-    //println(doc.source)
-    println(processed.sentences.head.tokens.mkString("\n"))
+    pipeline(doc)
+  }
 
+  def saveLoad(file:File) = try {
+    Some(load(file))
+  } catch {
+    case e:MalformedInputException =>
+      e.printStackTrace()
+      None
+  }
+
+  def loadDir(dir:File) = {
+    Util.files(dir) filter (_.getName.endsWith("txt.html")) flatMap saveLoad
+  }
+
+  def main(args: Array[String]) {
+    val file = new File("data/ohhla/www.ohhla.com/anonymous/j_live/allabove/satisfy.jlv.txt.html")
+    val processed = load(file)
+    //println(doc.source)
+    println(processed.sentences.map(_.toPrettyString).mkString("\n"))
+
+    val allAbove = loadDir(JLive.albumRoot)
+    println(allAbove.length)
 
   }
 }
