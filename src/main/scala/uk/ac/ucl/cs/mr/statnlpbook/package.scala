5c8bfb5792b8b43638bf8ad3581fea3ae9441c82
riedelcastro <sebastian.riedel@gmail.com>
2015-04-19 10:19:11 +0900

    Making it easier to tag/parse text

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/package.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/package.scala
@@ -1,6 +1,7 @@
 package uk.ac.ucl.cs.mr
 
 import ml.wolfe.nlp.Document
+import ml.wolfe.nlp.converters.SISTAProcessors
 import ml.wolfe.ui.BratRenderer
 
 /**
@@ -9,5 +10,8 @@ import ml.wolfe.ui.BratRenderer
 package object statnlpbook {
   def renderTokens(doc:Document) = BratRenderer.bratTokens(doc)
   def renderDependencies(doc:Document) = BratRenderer2.bratDependencies(doc)
+
+  def parse(text:String) =
+    SISTAProcessors.annotate(text,posTagger=true,parser=true)
 
 }
