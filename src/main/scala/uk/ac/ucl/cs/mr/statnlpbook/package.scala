c8fc2345a000d5149b0df589c17fb3082134db93
riedelcastro <sebastian.riedel@gmail.com>
2015-04-21 14:38:41 +0900

    convenience method to tokenize and segment text

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/package.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/package.scala
@@ -9,11 +9,16 @@ import org.sameersingh.htmlgen.{RawHTML, HTML}
  * @author riedel
  */
 package object statnlpbook {
+
+  lazy val segmentPipeline = Tokenizer.default andThen Segmenter.default
+
   def renderTokens(doc:Document) = BratRenderer2.bratTokens(doc)
   def renderDependencies(doc:Document) = BratRenderer2.bratDependencies(doc)
 
   def parse(text:String) =
     SISTAProcessors.annotate(text,posTagger=true,parser=true)
+
+  def segment(text:String) = segmentPipeline(Document.fromString(text))
 
   def raw(html:String) = RawHTML(html.replaceAll("sscript","script"))
 
