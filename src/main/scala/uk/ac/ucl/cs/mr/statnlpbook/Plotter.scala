3db16b17c5978a9e9d6d782b84321899a8fac30d
riedelcastro <sebastian.riedel@gmail.com>
2015-08-19 16:12:54 +0100

    added own line plotter and updated to new d3/nv version.

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/Plotter.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/Plotter.scala
@@ -13,15 +13,117 @@ import scala.collection.mutable.ArrayBuffer
  * @author riedel
  */
 object Plotter {
+  type XY = (Double, Double)
+
+  val colors = Seq("#ff7f0e", "#2ca02c", "#7777ff")
 
   def lineplot(chart: XYData):HTML = {
-    D3Plotter.lineplot(XYChartImplicits.dataToChart(chart))
+    lineplot(XYChartImplicits.dataToChart(chart))
   }
 
-  def lineplot(chart: XYChart):HTML = {
-    D3Plotter.lineplot(chart)
-  }
+  def lineplot(chart: XYChart) = {
+    val id = "d3line" + Math.abs(chart.hashCode()).toString
+    chart.data.serieses
 
+    def seriesDataToJson(series: XYSeries) = {
+      series.points.map(p => s"{x:${p._1}, y:${p._2}}").mkString("[", ",", "]")
+    }
+
+    def seriesToJson(series: XYSeries, color: String = colors.head) = s"""
+     |{
+     |values: ${seriesDataToJson(series)},
+     |key: "${series.name}",
+     |color: '$color',
+     |area: false
+     |}
+       """.stripMargin
+
+
+    def seriesWithColor(series: (XYSeries, Int)) = seriesToJson(series._1, colors(series._2 % colors.size))
+
+    val data = chart.data.serieses.zipWithIndex.map(seriesWithColor).mkString("[", ",", "]")
+
+    val style = s"""
+     |svg text {
+     |  fill: #ddd;
+     |}
+     |
+     |.tick line {
+     |  stroke: white;
+     |  opacity: 0.1;
+     |}
+     |
+     |#$id svg {
+     |  height: 400px;
+     |  width: 100%
+     |}
+      """.stripMargin
+
+
+    var labelIndex = 0
+    def xlabel(in: String) = if (in == "") "X" else in
+    def ylabel(in: String) = if (in == "") "Y" else in
+
+    val formats = """
+      |var logFormat = function(i) {
+      |    if (Math.log10(i) % 1 === 0)
+      |        return i
+      |     else
+      |        return "";
+      |};
+      |var linearFormat = d3.format('.02f');
+      |
+    """.stripMargin
+
+    val html = s"""
+       |<div id="$id">
+       |<svg></svg>
+       |</div>
+       |
+       |<style>
+       | $style
+       |</style>
+       |<script>
+       |
+       |/*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
+       |nv.addGraph(function() {
+       |var chart = nv.models.lineChart()
+       | .margin({left: 100, right:100})  //Adjust chart margins to give the x-axis some breathing room.
+       | .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+       | .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+       | .showYAxis(true)        //Show the y-axis
+       | .showXAxis(true)        //Show the x-axis
+       |;
+       |
+       | $formats
+       |
+       |chart.xAxis     //Chart x-axis settings
+       |.axisLabel("${xlabel(chart.x.label)}")
+       |.tickFormat(${if (chart.x.isLog) "logFormat" else "linearFormat"});
+       |
+       |chart.yAxis     //Chart y-axis settings
+       |.axisLabel("${ylabel(chart.y.label)}")
+       |.tickFormat(${if (chart.y.isLog) "logFormat" else "linearFormat"});
+       | ${if (chart.y.isLog) "chart.yScale(d3.scale.log());" else ""}
+       |
+       |/* Done setting the chart up? Time to render it!*/
+       |var myData = $data;   //You need data...
+       |
+       |d3.select('#$id svg')    //Select the <svg> element you want to render the chart in.
+       |.datum(myData)         //Populate the <svg> element with chart data...
+       |.transition().duration(350)
+       |.call(chart);          //Finally, render the chart!
+       |
+       |//Update the chart when window resizes.
+       |nv.utils.windowResize(function() { chart.update() });
+       |return chart;
+       |});
+       |</script>
+       |
+     """.stripMargin
+    println(html)
+    RawHTML(html)
+  }
 
   def barChart(map: Iterable[(Any, Double)], color:Option[String] = None) = {
     val id = "d3bar" + Math.abs(map.hashCode()).toString
