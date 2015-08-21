f000eaa595fbfd11d45e304eb5023fb8ebbf3b75
riedelcastro <sebastian.riedel@gmail.com>
2015-08-21 16:48:14 +0100

    started parsing chapter

---
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/chapter/Parsing.scala
@@ -0,0 +1,12 @@
+package uk.ac.ucl.cs.mr.statnlpbook.chapter
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ * @author riedelcastro
+ */
+object Parsing {
+  val current = new ListBuffer[String]
+  val test = Vector(1,2,3)
+
+}
