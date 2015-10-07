802097cf9df698f5fef2df7f321506cba07921ce
riedelcastro <sebastian.riedel@gmail.com>
2015-10-07 22:41:45 +0100

    fixes in LM

--- src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/chapter/languagemodels/LanguageModel.scala
+++ src/main/scala/uk/ac/ucl/cs/mr/statnlpbook/chapter/languagemodels/LanguageModel.scala
@@ -9,12 +9,107 @@ import scala.util.Random
 /**
  * @author riedel
  */
+/**
+ * An interface for a language model.
+ * @author riedel
+ */
 trait LanguageModel {
+  /**
+   * The length of history this LM considers.
+   * @return LM order.
+   */
   def order: Int
 
+  /**
+   * The set of words this LM is defined over.
+   * @return the vocabulary of this LM.
+   */
   def vocab: Set[String]
 
+  /**
+   * The probability p(word|history) this LM defines. For example, to calculate the probability that "sat" follows
+   * "the cat" you call probability("sat", "the", "cat").
+   * @param word a single word.
+   * @param history a var-arg sequence of words w_1, ... w_n-1 where n is the order of this LM and  w_n-1 is the
+   *                previous word and w_1 is the n-1th previous word.
+   * @return p(word|history).
+   */
   def probability(word: String, history: String*): Double
+}
+
+/**
+ * An LM that assigns uniform probability over all words of a vocabulary.
+ * @param vocab the vocabulary.
+ */
+case class UniformLM(vocab: Set[String]) extends LanguageModel {
+  def order = 1
+
+  def probability(word: String, history: String*) =
+    if (vocab(word)) 1.0 / vocab.size else 0.0
+}
+
+/**
+ * An LM that uses counts for each history and counts for each word given a history in order to calculate
+ * its probabilities.
+ */
+trait CountLM extends LanguageModel {
+  def counts: List[String] => Double
+
+  def norm: List[String] => Double
+
+  def probability(word: String, history: String*) = {
+    counts(word :: history.toList) / norm(history.toList)
+  }
+}
+
+/**
+ * An LM that
+ * @param train
+ * @param order
+ */
+case class NGramLM(train: IndexedSeq[String], order: Int) extends CountLM {
+  val vocab = train.toSet
+  val counts = new mutable.HashMap[List[String], Double] withDefaultValue 0.0
+  val norm = new mutable.HashMap[List[String], Double] withDefaultValue 0.0
+  for (i <- order until train.length) {
+    val history = train.slice(i - order + 1, i).toList
+    val word = train(i)
+    counts(word :: history) += 1.0
+    norm(history) += 1.0
+  }
+}
+
+case class LaplaceLM(base: CountLM, alpha: Double) extends CountLM {
+  def vocab = base.vocab
+
+  def order = base.order
+
+  def counts = h => base.counts(h) + alpha
+
+  def norm = h => base.norm(h) + alpha * base.vocab.size
+}
+
+case class LaplaceLMWithDiscounts(base: CountLM, alpha: Double) extends CountLM {
+  val eps = 0.001
+
+  def vocab = base.vocab
+
+  def order = base.order
+
+  def counts = h =>
+    (base.counts(h) + alpha) / (base.norm(h.tail) + alpha * base.vocab.size) * (base.norm(h.tail) + eps)
+
+  def norm = h => base.norm(h) + eps
+}
+
+case class InterpolatedLM(main: LanguageModel, backoff: LanguageModel, alpha: Double) extends LanguageModel {
+  def order = main.order
+
+  def vocab = main.vocab
+
+  def probability(word: String, history: String*) =
+    alpha * main.probability(word, history: _*) +
+      (1 - alpha) * backoff.probability(word, history.drop(1): _*)
 }
 
 object Util {
@@ -61,7 +156,8 @@ object Util {
         cross(t, appended)
     }
   }
-  def cross[T](args: List[List[T]], result: List[List[T]] = List(Nil)): List[List[T]] = revCross(args.reverse,result)
+
+  def cross[T](args: List[List[T]], result: List[List[T]] = List(Nil)): List[List[T]] = revCross(args.reverse, result)
 
 
 }
@@ -94,56 +190,4 @@ object LanguageModel {
   }
 
 
-
-}
-
-case class UniformLM(vocab: Set[String]) extends LanguageModel {
-  def order = 1
-
-  def probability(word: String, history: String*) =
-    if (vocab(word)) 1.0 / vocab.size else 0.0
-}
-
-trait CountLM extends LanguageModel {
-  def counts:List[String] => Double
-  def norm:List[String] => Double
-  def probability(word:String, history:String*) = {
-    counts(word :: history.toList) / norm(history.toList)
-  }
-}
-
-case class NGramLM(train:IndexedSeq[String],order:Int) extends CountLM {
-  val vocab = train.toSet
-  val counts = new mutable.HashMap[List[String],Double] withDefaultValue 0.0
-  val norm = new mutable.HashMap[List[String],Double] withDefaultValue 0.0
-  for (i <- order until train.length) {
-    val history = train.slice(i - order + 1, i).toList
-    val word = train(i)
-    counts(word :: history) += 1.0
-    norm(history) += 1.0
-  }
-}
-
-case class LaplaceLM(base:CountLM,alpha:Double) extends CountLM {
-  def vocab = base.vocab
-  def order = base.order
-  def counts = h => base.counts(h) + alpha
-  def norm = h => base.norm(h) + alpha * base.vocab.size
-}
-
-case class LaplaceLMWithDiscounts(base:CountLM,alpha:Double) extends CountLM {
-  val eps = 0.001
-  def vocab = base.vocab
-  def order = base.order
-  def counts = h =>
-    (base.counts(h) + alpha) / (base.norm(h.tail) + alpha * base.vocab.size) * (base.norm(h.tail) + eps)
-  def norm = h => base.norm(h) + eps
-}
-
-case class InterpolatedLM(main:LanguageModel, backoff:LanguageModel, alpha:Double) extends LanguageModel {
-  def order = main.order
-  def vocab = main.vocab
-  def probability(word:String, history:String*) =
-    alpha * main.probability(word,history:_*) +
-      (1 - alpha) * backoff.probability(word, history.drop(1):_*)
 }
