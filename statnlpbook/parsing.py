61a04fb9b22874880f8e775e63099fa5a866ac6b
riedelcastro <sebastian.riedel@gmail.com>
2016-06-19 10:36:32 +0900

    bottom up parsing implemented and visualised

--- statnlpbook/parsing.py
+++ statnlpbook/parsing.py
@@ -1,14 +1,15 @@
 from graphviz import Digraph
 
 
-def render_tree(tree):
+def render_forest(trees):
     """
-    Renders a (parse) tree using graphiz
+    Renders a (parse) tree forest using graphiz
     Args:
-        tree: recursive tree structure: tuple (node_label, children_nodes) for non-terminals or a string for terminals.
+        trees: list of recursive tree structure: tuple (node_label, children_nodes)
+               for non-terminals or a string for terminals.
 
     Returns:
-        the Digraph object representing the tree. Can be rendered in notebook.
+        the Digraph object representing the forest. Can be rendered in notebook.
     """
     nodes = []
     edges = []
@@ -25,7 +26,8 @@ def render_tree(tree):
             nodes.append((node_id, current))
         return nodes[-1]
 
-    collect_graph(tree)
+    for tree in trees:
+        collect_graph(tree)
     dot = Digraph(comment='The Round Table')
     for node_id, node_label in nodes:
         dot.node(node_id, node_label)
@@ -33,3 +35,35 @@ def render_tree(tree):
         dot.edge(arg1_id, arg2_id)
 
     return dot
+
+
+def render_tree(tree):
+    """
+    Renders a (parse) tree using graphiz
+    Args:
+        tree: recursive tree structure: tuple (node_label, children_nodes) for non-terminals or a string for terminals.
+
+    Returns:
+        the Digraph object representing the tree. Can be rendered in notebook.
+    """
+    return render_forest([tree])
+
+
+def render_transitions(transitions):
+    class Output:
+        def _repr_html_(self):
+            rows = ["<tr><td>{}</td><td>{}</td><td>{}</td></tr>".format(
+                " ".join(state.buffer),
+                render_forest(state.stack)._repr_svg_(),
+                action)
+                    for state, action in transitions]
+            return "<table>{}</table>".format("\n".join(rows))
+
+    return Output()
+
+
+def get_label(node):
+    if isinstance(node, str):
+        return node
+    else:
+        return node[0]
