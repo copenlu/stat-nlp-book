5d970c6532a37ecd977521399b0b5d3bb26b07d9
riedelcastro <sebastian.riedel@gmail.com>
2016-06-20 15:29:15 +0900

    cyk chart parsing and visualisation

--- statnlpbook/parsing.py
+++ statnlpbook/parsing.py
@@ -1,4 +1,5 @@
 from graphviz import Digraph
+from collections import defaultdict
 
 
 def render_forest(trees):
@@ -67,3 +68,110 @@ def get_label(node):
         return node
     else:
         return node[0]
+
+
+class Chart:
+    def __init__(self, sentence):
+        self.sentence = sentence
+        self.entries = defaultdict(list)  # map from (Int,Int,NonTerminal) to (list of (int,int,str) cells)
+        self.cell_to_entries = defaultdict(list)
+        self.target_cell_labels = set()
+        self.source_cell_labels = set()
+        self.target_cells = set()
+        self.source_cells = set()
+
+    def __getitem__(self, key):
+        return self.entries[key]
+
+    def __setitem__(self, key, value):
+        self.entries[key] = value
+        begin, end, label = key
+        self.cell_to_entries[begin, end] += [(label, value)]
+
+    def append_label(self, begin, end, label, sources=None):
+        self[begin, end, label] += [] if sources is None else sources
+
+    def entries_at_cell(self, begin, end):
+        return self.cell_to_entries[begin, end]
+
+    def labels_at_cell(self, begin, end):
+        return [label for label, _ in self.cell_to_entries[begin, end]]
+
+    def mark_target_label(self, begin, end, label):
+        self.target_cell_labels.add((begin, end, label))
+
+    def mark_source_label(self, begin, end, label):
+        self.source_cell_labels.add((begin, end, label))
+
+    def mark_source(self, begin, end):
+        self.source_cells.add((begin, end))
+
+    def mark_target(self, begin, end):
+        self.target_cells.add((begin, end))
+
+    def clear_label_marks(self):
+        self.target_cell_labels.clear()
+        self.source_cell_labels.clear()
+
+    def clear_cell_marks(self):
+        self.target_cells.clear()
+        self.source_cells.clear()
+
+    def clear_marks(self):
+        self.clear_cell_marks()
+        self.clear_label_marks()
+
+    def mark_cyk_focus(self, begin, middle, end):
+        self.clear_marks()
+        self.mark_target(begin, end)
+        self.mark_source(begin, middle)
+        self.mark_source(middle + 1, end)
+
+    def mark_cyk_rule(self, begin, end, alpha):
+        self.clear_label_marks()
+        self.mark(begin, end, alpha)
+
+    def mark_cyk_terminal(self, i, non_terminal):
+        self.clear_label_marks()
+        self.mark_target_label(i, i, non_terminal)
+
+    def mark(self, begin, end, label):
+        self.mark_target_label(begin, end, label)
+        for b, e, l in self[begin, end, label]:
+            self.mark_source_label(b, e, l)
+
+    def _repr_html_(self):
+        header = "<tr><td></td>{}</tr>".format(
+            "".join(["<td>{}: {}</td>".format(i, self.sentence[i]) for i in range(0, len(self.sentence))]))
+        rows = [header]
+
+        def color_for_label(b, e, l):
+            if (b, e, l) in self.target_cell_labels:
+                return 'blue'
+            elif (b, e, l) in self.source_cell_labels:
+                return 'red'
+            else:
+                return 'black'
+
+        def color_for_cell(b, e):
+            if (b, e) in self.target_cells:
+                return '#C3C3C3'
+            elif (b, e) in self.source_cells:
+                return '#E1E1E1'
+            else:
+                return 'white'
+
+        for row_index in range(0, len(self.sentence)):
+            cells = []
+            for col_index in range(0, len(self.sentence)):
+                relevant_entries = self.cell_to_entries[row_index, col_index]
+                border = 'solid' if col_index >= row_index else 'none'
+                labels = ['<font color="{}">{}</font>'.format(color_for_label(row_index, col_index, label), label) for
+                          label, _ in relevant_entries]
+                cell_color = color_for_cell(row_index, col_index)
+                cell = """<td style="border:{};" bgcolor="{}">{}</td>""".format(border, cell_color,
+                                                                                ", ".join(labels))
+                cells.append(cell)
+            row = "<tr><td>{}: {}</td>{}</tr>".format(row_index, self.sentence[row_index], "".join(cells))
+            rows.append(row)
+        return "<table>{}</table>".format("\n".join(rows))
