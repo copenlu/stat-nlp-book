d0563b5846b17fbcbf6c7ceea27f27c3c58793cc
riedelcastro <sebastian.riedel@gmail.com>
2016-08-04 16:21:49 +0100

    MEMM model

--- statnlpbook/sequence.py
+++ statnlpbook/sequence.py
@@ -76,7 +76,7 @@ def draw_local_fg(length=3):
     return result
 
 
-def draw_mm_fg(length=3):
+def draw_transition_fg(length=3):
     var_style = {'shape': 'circle'}
     factor_style = {'shape': 'box', 'fontcolor': 'white',
                     'style': 'filled', 'fillcolor': 'black', 'width': '0.2', 'height': '0.2'}
@@ -102,6 +102,36 @@ def draw_mm_fg(length=3):
     return result
 
 
+def draw_memm_fg(length=3):
+    var_style = {'shape': 'circle'}
+    factor_style = {'shape': 'box', 'fontcolor': 'white',
+                    'style': 'filled', 'fillcolor': 'black', 'width': '0.2', 'height': '0.2'}
+
+    ys = graph()
+    result = graph()
+    result.node("x", shape='circle', style='filled', fillcolor='lightgrey')
+    result.node("t0", **factor_style)
+    for i in range(0, length):
+        node_id = "y" + str(i)
+        factor_id = "l" + str(i)
+        ys.node(node_id, **var_style)
+        # result.node(factor_id, **factor_style)
+        # result.edge(node_id, factor_id)
+        # result.edge(factor_id, "x")
+    result.edge("t0", "x")
+    result.edge("t0", "y0")
+
+    for i in range(1, length):
+        factor_id = "t" + str(i)
+        ys.node(factor_id, **factor_style)
+        result.edge("x", factor_id, constraint="false")
+        ys.edge("y" + str(i), factor_id, constraint="true")
+        ys.edge("y" + str(i - 1), factor_id, constraint="true")
+
+    result.subgraph(ys)
+    return result
+
+
 def to_classifier_x(data, f):
     return [f(x, i) for x, _ in data for i in range(0, len(x))]
 
@@ -116,15 +146,15 @@ class LocalSequenceLabeler:
         self.feat = feat
         self.vectorizer = DictVectorizer()
         self.label_encoder = LabelEncoder()
-        train_classifier_X = self.vectorizer.fit_transform(to_classifier_x(train_data, feat))
-        train_classifier_Y = self.label_encoder.fit_transform(to_classifier_y(train_data))
+        train_classifier_x = self.vectorizer.fit_transform(to_classifier_x(train_data, feat))
+        train_classifier_y = self.label_encoder.fit_transform(to_classifier_y(train_data))
         self.lr = LogisticRegression(**lr_params)
-        self.lr.fit_transform(train_classifier_X, train_classifier_Y)
+        self.lr.fit_transform(train_classifier_x, train_classifier_y)
 
-    def plot_lr_weights(self, label, how_many=20):
+    def plot_lr_weights(self, label, how_many=20, reverse=True):
         v_index = self.label_encoder.transform(label)
         v_weights = self.vectorizer.inverse_transform(self.lr.coef_)[v_index]
-        sorted_weights = sorted(v_weights.items(), key=lambda t: t[1], reverse=True)
+        sorted_weights = sorted(v_weights.items(), key=lambda t: t[1], reverse=reverse)
         return util.plot_bar_graph([w for _, w in sorted_weights[:how_many]],
                                    [f for f, _ in sorted_weights[:how_many]], rotation=45)
 
@@ -145,6 +175,83 @@ class LocalSequenceLabeler:
                     print("Guess: {}".format(y_guess[i]))
                     print(" ".join(x[max(0, i - 5):i]) + " [" + x[i] + "] " + " ".join(x[i + 1:min(i + 5, len(x))]))
                     print(self.feat(x, i))
+
+
+def padded_history(seq, index, length):
+    history = tuple(seq[max(0, index - length):index])
+    padding = (length - len(history)) * ("PAD",)
+    return padding + history
+
+
+class MEMMSequenceLabeler:
+    def transform_input(self, data):
+        return [self.feat(x, i, padded_history(y, i, self.order)) for x, y in data for i in range(0, len(x))]
+
+    def __init__(self, feat, train_data, order=1, **lr_params):
+        self.order = order
+        self.train_data = train_data
+        self.feat = feat
+        self.vectorizer = DictVectorizer()
+        self.label_encoder = LabelEncoder()
+
+        train_classifier_x = self.vectorizer.fit_transform(self.transform_input(train_data))
+        train_classifier_y = self.label_encoder.fit_transform(to_classifier_y(train_data))
+        self.lr = LogisticRegression(**lr_params)
+        self.lr.fit_transform(train_classifier_x, train_classifier_y)
+
+    def plot_lr_weights(self, label, how_many=20, reverse=True, feat_filter=lambda s: True):
+        v_index = self.label_encoder.transform(label)
+        v_weights = self.vectorizer.inverse_transform(self.lr.coef_)[v_index]
+        # print(type(v_weights.items()))
+        filtered = [(k, v) for k, v in v_weights.items() if feat_filter(k)]
+        sorted_weights = sorted(filtered, key=lambda t: t[1],
+                                reverse=reverse)
+        return util.plot_bar_graph([w for _, w in sorted_weights[:how_many]],
+                                   [f for f, _ in sorted_weights[:how_many]], rotation=45)
+
+    def input_repr(self, x, i, y):
+        return self.feat(x, i, padded_history(y, i, self.order))
+
+    def predict_next(self, x, i, y):
+        scikit_x = self.vectorizer.transform([self.input_repr(x, i, y)])
+        return self.label_encoder.inverse_transform(self.lr.predict(scikit_x))[0]
+
+    def predict(self, data):
+        result = []
+        for x, y in data:
+            y_guess = []
+            for i in range(0, len(x)):
+                prediction = self.predict_next(x, i, y_guess)
+                y_guess += prediction
+            result.append(y_guess)
+        return result
+        # dev_classifier_Y = label_encoder.transform(to_classifier_y(dev))
+        # dev_classifier_output = self.label_encoder.inverse_transform(
+        #     self.lr.predict(self.vectorizer.transform(to_classifier_x(data, self.feat))))
+        # dev_output = to_xy(data, dev_classifier_output)
+        # return dev_output
+
+        # def show_errors(self, data, filter_gold=lambda y: True, filter_guess=lambda y: True):
+        #     guess = self.predict(data)
+        #     for (x, y), y_guess in zip(data, guess):
+        #         for i in range(0, len(y)):
+        #             if y[i] != y_guess[i] and filter_gold(y[i]) and filter_guess(y_guess[i]):
+        #                 print("---------")
+        #                 print("Gold:  {}".format(y[i]))
+        #                 print("Guess: {}".format(y_guess[i]))
+        #                 print(" ".join(x[max(0, i - 5):i]) + " [" + x[i] + "] " + " ".join(x[i + 1:min(i + 5, len(x))]))
+        #                 print(self.feat(x, i))
+
+
+def memm_greedy_predict(memm: MEMMSequenceLabeler, data):
+    result = []
+    for x, y in data:
+        y_guess = []
+        for i in range(0, len(x)):
+            prediction = memm.predict_next(x, i, y_guess)
+            y_guess += prediction
+        result.append(y_guess)
+    return result
 
 
 def accuracy(gold, guess):
