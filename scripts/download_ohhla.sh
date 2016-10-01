b759197135dfed1d4dc2499060955450822a22a0
riedelcastro <sebastian.riedel@gmail.com>
2016-10-01 13:19:51 +0100

    assignment as notebook

--- scripts/download_ohhla.sh
+++ scripts/download_ohhla.sh
@@ -3,10 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOWNLOAD=$DIR/download_artist.sh
 
-$DOWNLOAD http://www.ohhla.com/YFA_roots.html $DIR/../data/ohhla
-$DOWNLOAD http://www.ohhla.com/YFA_rakim.html $DIR/../data/ohhla
-$DOWNLOAD http://www.ohhla.com/YFA_atcq.html $DIR/../data/ohhla
-$DOWNLOAD http://www.ohhla.com/YFA_gsr.html $DIR/../data/ohhla
-$DOWNLOAD http://www.ohhla.com/YFA_slickrick.html $DIR/../data/ohhla
-$DOWNLOAD http://www.ohhla.com/YFA_nas.html $DIR/../data/ohhla
-$DOWNLOAD www.ohhla.com/anonymous/j_live $DIR/../data/ohhla
+$DOWNLOAD http://www.ohhla.com/YFA_roots.html $DIR/../data/ohhla/train
+$DOWNLOAD http://www.ohhla.com/YFA_rakim.html $DIR/../data/ohhla/train
+$DOWNLOAD http://www.ohhla.com/YFA_atcq.html $DIR/../data/ohhla/train
+$DOWNLOAD http://www.ohhla.com/YFA_gsr.html $DIR/../data/ohhla/train
+$DOWNLOAD http://www.ohhla.com/YFA_slickrick.html $DIR/../data/ohhla/train
+$DOWNLOAD http://www.ohhla.com/YFA_nas.html $DIR/../data/ohhla/train
+$DOWNLOAD www.ohhla.com/anonymous/j_live $DIR/../data/ohhla/train
+$DOWNLOAD www.ohhla.com/YFA_common.html $DIR/../data/ohhla/dev
