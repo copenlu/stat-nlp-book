6eac505a3d2d86e57d382180da0d4684ffd79e74
riedelcastro <sebastian.riedel@gmail.com>
2016-10-16 22:45:46 +0100

    Fixes

--- statnlpbook/mt.py
+++ statnlpbook/mt.py
@@ -5,7 +5,7 @@ def create_translation_table(source_vocab, target_vocab, fixed=None):
     for s in source_vocab:
         for t in target_vocab:
             if t in fixed:
-                alpha[s, t] = 1.0 if fixed[t] == s else 0.0
+                alpha[s, t] = fixed[t].get(s, 0.0)
             else:
                 alpha[s, t] = 1.0 / len(source_vocab)
     return alpha
@@ -19,8 +19,8 @@ def create_distortion_table(max_length, fixed=None):
         for si in range(0, max_length):
             for lt in range(1, max_length + 1):
                 for ls in range(1, max_length + 1):
-                    if ti in fixed:
-                        beta[ti, si, lt, ls] = 1.0 if fixed[ti] == si else 0.0
+                    if si in fixed:
+                        beta[ti, si, lt, ls] = 1.0 if fixed[si] == ti else 0.0
                     else:
                         beta[ti, si, lt, ls] = 1.0 / lt
     return beta
