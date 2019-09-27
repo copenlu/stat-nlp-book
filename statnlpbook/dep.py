c284210c3dc013f28b0210f5e949925eda5feb30
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2019-09-27 15:55:22 +0200

    Simpler CoNLL-U code

---
+++ statnlpbook/dep.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+
+def load_conllu(file_path):
+    """
+    Load a given CoNLL-U file and return a sequence of Python object or dict representing parsed sentences.
+    Args:
+        file_path: The file to read from, e.g. data/ud/en_ewt-ud-train.conllu or data/ud/da_ddt-ud-dev.conllu
+    Returns:
+        A sequence of Python object or dict representing parsed sentences.
+    """
+    trees = []
+    tree = None
+    with open(file_path, encoding="utf-8") as f:
+        for line_no, line in enumerate(list(f) + [""]):  # Append empty line to handle last
+            try:
+                line = line.strip()
+                if not line:  # Empty line
+                    if tree:  # Finalize tree and add to list
+                        for node in tree["nodes"][1:]:  # Add this node to head's dependents list
+                            tree["nodes"][int(node["head"])]["dependents"].append(node)
+                        trees.append(tree)
+                    tree = None  # Prepare to read a new sentence
+                elif tree is None:  # Creating a new tree
+                    root = {"index": "0", "form": "ROOT", "dependents": []}
+                    tree = {"nodes": [root]}  # Initialize node list
+                elif line.startswith("#"):  # Handle sent_id and text comments
+                    key, _, value = line[1:].strip().partition(" = ")
+                    tree[key] = value
+                else:  # Read columns into new node
+                    node = {"dependents": []}
+                    node["index"], node["form"], node["lemma"], node["upos"], \
+                        node["xpos"], _, node["head"], node["deprel"], _, _ = line.split("\t")
+                    if not {".", "-"}.intersection(node["index"]):  # Skip special nodes
+                        tree["nodes"].append(node)
+            except:
+                raise IOError("Failed loading line %d in '%s':\n%s" % (line_no, file_path, line))
+    return trees
+
+
+def save_to_conllu(data, file_path):
+    """
+    Save a Python object or dict representing a sequence of parsed sentence in back to a .conllu file.
+    Args:
+        data: The parsed sentences.
+        datadir: The file to save to, e.g. data/ud/predictions.conllu
+    """
+    with open(file_path, "w", encoding="utf-8") as f:
+        for tree in data:
+            for key, value in tree.items():  # Write comments
+                if key == "nodes":
+                    continue
+                print("# " + key + " = " + value, file=f)
+            for node in tree["nodes"][1:]:  # Skip the root
+                print(node["index"], node["form"], node["lemma"], node["upos"],
+                      node["xpos"], "_", node["head"], node["deprel"], "_", "_",
+                      sep="\t", file=f)
+            print(file=f)  # Separate sentences by empty line
+
+
+def evaluate_las(gold, pred):
+    """
+    Save a Python object or dict representing a sequence of parsed sentence in back to a .conllu file.
+    Args:
+        gold: The gold-standard sentences or file containing them.
+        pred: The parsed sentences or file containing them.
+    Returns:
+        LAS (labeled attachment score) averaged across all sentences.
+    """
+    gold = try_load_conllu(gold)
+    pred = try_load_conllu(pred)
+    total = correct = 0
+    for gold_tree, pred_tree in zip(gold, pred):
+        for gold_node, pred_node in zip(gold_tree["nodes"][1:], pred_tree["nodes"][1:]):
+            total += 1
+            if gold_node["head"] == pred_node["head"] and gold_node["deprel"] == pred_node["deprel"]:
+                correct += 1
+    return correct / total
+
+
+def try_load_conllu(file_or_sentences):
+    try:
+        return load_conllu(file_or_sentences)
+    except TypeError:
+        return file_or_sentences
+
+
+if __name__ == "__main__":
+    import os
+    dev_file_path = os.path.join("data", "ud", "da_ddt-ud-dev.conllu")
+    dev_data = load_conllu(dev_file_path)
+    dev_copy_file_path = os.path.join("data", "ud", "da_ddt-ud-dev_copy.conllu")
+    save_to_conllu(dev_data, dev_copy_file_path)
+    print(evaluate_las(dev_file_path, dev_copy_file_path))
+    train_file_path = os.path.join("data", "ud", "en_ewt-ud-train.conllu")
+    train_data = load_conllu(train_file_path)
+    train_copy_file_path = os.path.join("data", "ud", "en_ewt-ud-train_copy.conllu")
+    save_to_conllu(train_data, train_copy_file_path)
+    print(evaluate_las(train_file_path, train_copy_file_path))
